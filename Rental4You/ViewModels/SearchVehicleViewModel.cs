66104bf216fbfb301001d07da0886de652891307
Yeshey <41551785+Yeshey@users.noreply.github.com>
2022-12-03 00:05:13 +0000

    Added little tmp search box in home2

---
+++ Rental4You/ViewModels/SearchVehicleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Rental4You.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Rental4You.ViewModels
+{
+    public class SearchVehicleViewModel
+    {
+        public List<Vehicle> VehicleList { get; set; }
+        public int NumResults { get; set; }
+        [Display(Name = "SEARCH FOR VEHICLES ...", Prompt = "Incert text to search...")]
+        public string TextToSearch { get; set; }
+
+    }
+}
