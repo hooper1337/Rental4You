c09c03bdacc5dfb4840feedfa7267e34b1fcf477
Yeshey <41551785+Yeshey@users.noreply.github.com>
2023-01-03 13:59:44 +0000

    (DATABASE CHANGE) same thing but better

---
+++ Rental4You/Migrations/20230103135720_deliverandretrivaldetails.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rental4You.Migrations
+{
+    public partial class deliverandretrivaldetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DamageDelivery",
+                table: "reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DamageRetrieval",
+                table: "reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "reservations",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployerDelivery",
+                table: "reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployerRetrieval",
+                table: "reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfKmOfVehicleDelivery",
+                table: "reservations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfKmOfVehicleRetrieval",
+                table: "reservations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ObservationsDelivery",
+                table: "reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ObservationsRetrieval",
+                table: "reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RetrievalDate",
+                table: "reservations",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DamageDelivery",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "DamageRetrieval",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryDate",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "EmployerDelivery",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "EmployerRetrieval",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfKmOfVehicleDelivery",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfKmOfVehicleRetrieval",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "ObservationsDelivery",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "ObservationsRetrieval",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "RetrievalDate",
+                table: "reservations");
+        }
+    }
+}
